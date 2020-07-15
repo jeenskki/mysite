d1b78ea216ed3295f91083cb56bca03078d7ea21
jeenskki <madrine27@naver.com>
2020-07-15 09:26:35 +0900

    fixed bugs

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Question
+from .models import Question, Choice
 from django.urls import reverse
 
 # Create your views here.
